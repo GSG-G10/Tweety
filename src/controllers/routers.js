507ce3b29b9e6b7f1ff987e73431ee87e2bf56ff
Abdallah <dev.aaamra@gmail.com>
2021-08-10 23:11:37 +0300

    error pages functionality

--- src/controllers/routers.js
+++ src/controllers/routers.js
@@ -4,11 +4,15 @@ const path = require('path');
 const router = express.Router();
 
 
-const mainRoute =  require('./main.js');
-const searchRouter =  require('./search.js');
+const mainRoute =  require('./main');
+const searchRouter =  require('./search');
 
+const error = require('./error');
 
 router.get('/', mainRoute);
 router.get('/search/:userName', searchRouter);
+
+router.use(error.notFound);
+router.use(error.serverError);
 
 module.exports= router;
